a17f9680654a98ee37592d104aad1fe6f64e5b9a
Xiaohan Wang <xiaohan_wang@unc.edu>
2023-04-22 15:33:47 -0400

    puzzleimpl

--- src/main/java/com/comp301/a09akari/model/PuzzleImpl.java
+++ src/main/java/com/comp301/a09akari/model/PuzzleImpl.java
@@ -1,51 +1,51 @@
 package com.comp301.a09akari.model;
 
 public class PuzzleImpl implements Puzzle {
-    private int[][] board;
+  private int[][] board;
 
-    public PuzzleImpl(int[][] board) {
-        this.board = board;
+  public PuzzleImpl(int[][] board) {
+    this.board = board;
+  }
+
+  @Override
+  public int getWidth() {
+    return board[0].length;
+  }
+
+  @Override
+  public int getHeight() {
+    return board.length;
+  }
+
+  @Override
+  public CellType getCellType(int row, int col) {
+    if (row >= getHeight() || col >= getWidth() || row < 0 || col < 0) {
+      throw new IndexOutOfBoundsException();
     }
+    int cell = board[row][col];
 
-    @Override
-    public int getWidth() {
-        return board[0].length;
+    if (cell < 5) {
+      return CellType.CLUE;
     }
-
-    @Override
-    public int getHeight() {
-        return board.length;
+    if (cell == 5) {
+      return CellType.WALL;
     }
-
-    @Override
-    public CellType getCellType(int row, int col) {
-        if (row >= getHeight() || col >= getWidth() || row < 0 || col < 0) {
-            throw new IndexOutOfBoundsException();
-        }
-        int cell = board[row][col];
-
-        if (cell < 5) {
-            return CellType.CLUE;
-        }
-        if (cell == 5) {
-            return CellType.WALL;
-        }
-        if (cell == 6) {
-            return CellType.CORRIDOR;
-        }
-        return null;
+    if (cell == 6) {
+      return CellType.CORRIDOR;
     }
+    return null;
+  }
 
-    @Override
-    public int getClue(int row, int col) {
-        if (row >= getHeight() || col >= getWidth() || row < 0 || col < 0) {
-            throw new IndexOutOfBoundsException();
-        }
-        int cell = board[row][col];
-
-        if (cell > 4) {
-            throw new IllegalArgumentException();
-        }
-        return cell;
+  @Override
+  public int getClue(int row, int col) {
+    if (row >= getHeight() || col >= getWidth() || row < 0 || col < 0) {
+      throw new IndexOutOfBoundsException();
     }
+    int cell = board[row][col];
+
+    if (cell > 4) {
+      throw new IllegalArgumentException();
+    }
+    return cell;
+  }
 }
